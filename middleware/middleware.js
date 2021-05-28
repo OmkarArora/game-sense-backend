b7d3ca88bd49d21a16e974987ae5f3996d5bed0e
Omkar Arora <aroraomkar@gmail.com>
2021-05-28 20:31:07 +0530

    refactor: code formatted

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -4,25 +4,25 @@ const requestInfo = (req, res, next) => {
   console.log("\nREQUEST", req.path);
   console.log("METHOD", req.method);
   next();
-}
+};
 
 const paramLogger = (req, res, next) => {
-  if(req.params){
+  if (req.params) {
     console.log("\nPARAMS");
     console.table(req.params);
     req.paramsChecked = true;
-  }else{
+  } else {
     req.paramsChecked = false;
   }
   next();
-}
+};
 
 const authVerify = (req, res, next) => {
   const token = req.headers.authorization;
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.userAuth = { userId: decoded.userId, email: decoded.email };
-    if(decoded.userId !== String(req.user._id)){
+    if (decoded.userId !== String(req.user._id)) {
       return res.status(401).json({
         success: false,
         message: "User authentication failed",
@@ -39,7 +39,10 @@ const authVerify = (req, res, next) => {
 
 const errorHandler = (err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json({success: false, message: "error occurred, see the error message for more details"});
-}
+  res.status(500).json({
+    success: false,
+    message: "error occurred, see the error message for more details",
+  });
+};
 
-module.exports = { requestInfo, paramLogger, errorHandler, authVerify }
+module.exports = { requestInfo, paramLogger, errorHandler, authVerify };
